cc74c5c1313c5c62c57c53cfdfc10722854dff41
Carlos Daniel <c4rl1t0sd4n1e1@gmail.com>
2025-01-17 19:40:41 -0400

    Renaming the root directory

--- server/config.js
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-export const PORT = process.env.PORT || 4000;
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const MONGODB_URI = process.env.MONGODB_URI;
-export const FRONTEND_URL = process.env.FRONTEND_URL;
